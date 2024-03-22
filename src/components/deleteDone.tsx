8fe861acd2ba50d9571d0ab9dff810849c91a7e8
Thiago <thiagocrrabelo@gmail.com>
2024-03-22 01:33:37 -0300

    Create: Component deleteDone

---
+++ src/components/deleteDone.tsx
@@ -0,0 +1,7 @@
+export default function DeleteDone() {
+  return (
+    <button className="bg-red-600 text-white border-2 border-black rounded-lg font-bold px-6 py-0.5">
+      Delete done
+    </button>
+  );
+}
