81687d00493dc6c6a388edd74f9b9dbe2af5907a
Thiago <thiagocrrabelo@gmail.com>
2024-03-22 02:43:32 -0300

    Update: Component filter

--- src/components/filter.tsx
+++ src/components/filter.tsx
@@ -16,7 +16,7 @@ export default function Filter() {
   return (
     <select
       name="filter"
-      className="appearance-none rounded-lg text-center w-64 py-2 font-medium hover:cursor-pointer hover:bg-primary hover:text-neutral border-2 border-black outline-none"
+      className="appearance-none bg-neutral rounded-lg text-center w-64 py-2 font-medium hover:cursor-pointer hover:bg-primary hover:text-neutral border-2 border-black outline-none"
     >
       <Options />
     </select>
