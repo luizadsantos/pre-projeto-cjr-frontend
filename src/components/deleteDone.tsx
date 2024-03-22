52cb8ebf6649d0250c36612cd7292302f9a5baa0
Thiago <thiagocrrabelo@gmail.com>
2024-03-22 02:39:29 -0300

    Update: Component deleteDone color palette

--- src/components/deleteDone.tsx
+++ src/components/deleteDone.tsx
@@ -1,6 +1,6 @@
 export default function DeleteDone() {
   return (
-    <button className="bg-red-600 text-white border-2 border-black rounded-lg font-bold px-6 py-0.5">
+    <button className="border-2 border-black rounded-lg font-bold hover:bg-primary hover:text-neutral px-6 py-0.5">
       Delete done
     </button>
   );
