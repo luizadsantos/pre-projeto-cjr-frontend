a752d466664985377ef51bdd2b797bb04b0f5c93
Thiago <thiagocrrabelo@gmail.com>
2024-03-22 02:38:40 -0300

    Update: Layout color palette

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={"min-h-screen bg-none bg-white text-black"}>
+      <body className={"min-h-screen bg-none bg-neutral text-black"}>
         {children}
       </body>
     </html>
