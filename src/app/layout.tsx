c0d32482204df2742f972952dc705b4a2456aa84
Thiago <thiagocrrabelo@gmail.com>
2024-03-22 09:37:57 -0300

    Fix: Background gradient

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,9 +13,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={"min-h-screen bg-none bg-neutral text-black"}>
-        {children}
-      </body>
+      <body className={"min-h-screen bg-neutral text-black"}>{children}</body>
     </html>
   );
 }
