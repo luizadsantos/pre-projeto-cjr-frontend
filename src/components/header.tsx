92d41420b5ed75a89a4d5f64c7a883ce6966ed39
Thiago <thiagocrrabelo@gmail.com>
2024-03-22 02:39:53 -0300

    Update: Component header color palette

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,9 +1,11 @@
 export default function Header() {
   return (
-    <header className="flex justify-between p-8 py-6 font-bold bg-black">
-      <h1 className="text-3xl text-white">To-Do list</h1>
+    <header className="flex items-center justify-between p-8 py-6 font-bold bg-primary">
+      <h1 className="text-3xl text-neutral">To-Do list</h1>
 
-      <button className="rounded-lg bg-white p-2">Create task</button>
+      <button className="rounded-lg bg-neutral border-2 border-black hover:bg-primary hover:text-neutral p-2">
+        Create task
+      </button>
     </header>
   );
 }
