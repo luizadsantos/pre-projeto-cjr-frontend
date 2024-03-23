b278a613e88011eb4ab5b988a1519016211ecc77
Thiago <thiagocrrabelo@gmail.com>
2024-03-23 01:37:51 -0300

    Update: Models category

--- src/models/category.ts
+++ src/models/category.ts
@@ -4,3 +4,17 @@ export interface Category {
   createdAt: string;
   updatedAt: string;
 }
+
+export interface CreateCategory {
+  id?: number;
+  name: string;
+  createdAt?: string;
+  updatedAt?: string;
+}
+
+export interface UpdateCategory {
+  id: number;
+  name: string;
+  createdAt?: string;
+  updatedAt?: string;
+}
