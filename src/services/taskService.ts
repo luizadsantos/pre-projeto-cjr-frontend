ca01cf9102d6e9a4c1bce96a7a256e0265d13af0
Thiago <thiagocrrabelo@gmail.com>
2024-03-23 01:38:45 -0300

    Update: Services task

--- src/services/taskService.ts
+++ src/services/taskService.ts
@@ -1,21 +1,107 @@
-import { Task } from "@/models/task";
+import { CreateTask, Task, UpdateTask } from "@/models/task";
 import axios from "axios";
 
 export const axiosInstance = axios.create({
   baseURL: "http://localhost:3000",
 });
 
+function throwError(response: {
+  status: number;
+  error?: string;
+  message?: string;
+}) {
+  if (response.status !== 200)
+    throw new Error(
+      response.error && response.message
+        ? response.status + ": " + response.error + "\n" + response.message
+        : "Failed to fetch tasks",
+    );
+}
+
+interface ResponseTask {
+  data: { data: Task };
+  message?: string;
+  error?: string;
+  status: number;
+}
+
+interface ResponseTasks {
+  data: { data: Task[] };
+  message?: string;
+  error?: string;
+  status: number;
+}
+
 export class TaskService {
   async getTasks() {
-    const response = (await axiosInstance.get("/task")) as {
-      data: { data: Task[] };
-      status: number;
-    };
+    const response = (await axiosInstance.get("/task")) as ResponseTasks;
 
-    if (response.status !== 200)
-      throw new Error(
-        "Failed to fetch tasks. Status: " + response.status.toString(),
-      );
+    throwError(response);
+
+    return response.data.data;
+  }
+
+  async getTaskById(id: number) {
+    const response = (await axiosInstance.get(
+      "/task/" + id.toString(),
+    )) as ResponseTask;
+
+    throwError(response);
+
+    return response.data.data;
+  }
+
+  async createTask(task: CreateTask) {
+    const response = (await axiosInstance.post("/task", task)) as ResponseTask;
+
+    throwError(response);
+
+    return response.data.data;
+  }
+
+  async updateTask(task: UpdateTask) {
+    const response = (await axiosInstance.patch(
+      "/task/" + task.id.toString(),
+      task,
+    )) as ResponseTask;
+
+    throwError(response);
+
+    return response.data.data;
+  }
+
+  async deleteTask(id: number) {
+    const response = (await axiosInstance.delete(
+      "/task/" + id.toString(),
+    )) as ResponseTask;
+
+    throwError(response);
+
+    return response.data.data;
+  }
+
+  async deleteDoneTasks() {
+    const response = (await axiosInstance.delete("/task")) as ResponseTasks;
+
+    throwError(response);
+
+    return response.data.data;
+  }
+
+  async getNotDoneTasks() {
+    const response = (await axiosInstance.get("/task/active")) as ResponseTasks;
+
+    throwError(response);
+
+    return response.data.data;
+  }
+
+  async getTaskByCategory(categoryId: number) {
+    const response = (await axiosInstance.get(
+      "/task/category/" + categoryId.toString(),
+    )) as ResponseTask;
+
+    throwError(response);
 
     return response.data.data;
   }
