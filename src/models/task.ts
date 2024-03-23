2dcb3b4d520602f4b39668386007b5a5bf2cbc23
Thiago <thiagocrrabelo@gmail.com>
2024-03-23 01:38:08 -0300

    Update: Models task

--- src/models/task.ts
+++ src/models/task.ts
@@ -6,3 +6,21 @@ export interface Task {
   createdAt: string;
   updatedAt: string;
 }
+
+export interface CreateTask {
+  id?: number;
+  name: string;
+  isActive?: boolean;
+  categoryId?: number;
+  createdAt?: string;
+  updatedAt?: string;
+}
+
+export interface UpdateTask {
+  id: number;
+  name?: string;
+  isActive?: boolean;
+  categoryId?: number;
+  createdAt?: string;
+  updatedAt?: string;
+}
