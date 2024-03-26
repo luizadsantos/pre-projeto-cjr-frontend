b601cce90290f5a8ca243315260687c6cb1e4866
Thiago <thiagocrrabelo@gmail.com>
2024-03-26 13:51:52 -0300

    Update: Utils updateData

--- src/utils/updateData.ts
+++ src/utils/updateData.ts
@@ -6,16 +6,27 @@ import { Dispatch, SetStateAction } from "react";
 import showError from "./errorHandling";
 
 export default async function fetchData({
+  filter,
   setTasks,
   setCategories,
 }: {
+  filter?: "all" | "done" | "pending";
   setTasks?: Dispatch<SetStateAction<Task[]>>;
   setCategories?: Dispatch<SetStateAction<Category[]>>;
 }) {
   const taskServices = new TaskService();
   const categoryServices = new CategoryService();
   try {
-    if (setTasks) setTasks(await taskServices.getTasks());
+    if (setTasks)
+      if (!filter || filter == "all") {
+        setTasks(await taskServices.getTasks());
+      } else {
+        setTasks(
+          await (filter == "done"
+            ? taskServices.getDoneTasks()
+            : taskServices.getNotDoneTasks()),
+        );
+      }
     if (setCategories) setCategories(await categoryServices.getCategories());
   } catch (error) {
     showError(error);
