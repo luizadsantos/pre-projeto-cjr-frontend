8dc7f752bdfca51a307405a54b786873334aa5b4
Thiago <thiagocrrabelo@gmail.com>
2024-03-26 15:49:20 -0300

    Update: Component confirm

--- src/components/confirm.tsx
+++ src/components/confirm.tsx
@@ -10,7 +10,7 @@ export default function Confirm({
   setShow: Dispatch<SetStateAction<boolean>>;
 }) {
   return (
-    <div className="fixed left-0 top-0 w-full flex items-center justify-center h-full overflow-hidden bg-black">
+    <div className="fixed left-0 top-0 w-full flex items-center justify-center h-full overflow-hidden bg-opacity-40 bg-black">
       <div className="flex flex-col gap-4 p-4 rounded-lg items-center opacity-100 w-2/3 bg-neutral">
         <h1 className="text-4xl font-bold text-center">{message}</h1>
         <div className="flex gap-4">
